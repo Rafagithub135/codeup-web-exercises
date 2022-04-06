957c6a2d01f0d40f095182802098813fd66efd21
Ralph <mccloskey.ralph35@gmail.com>
2022-04-06 19:17:24 -0400

    Contractor code added

--- js/external.js
+++ js/external.js
@@ -17,3 +17,17 @@ var totalPaid = Number(pricePerDay * totalNumberOfDays);
 alert("Your total number of days you rented movies was " + totalNumberOfDays +
     " and the total price you paid was $" + totalPaid + ".")
 
+var hourlyWageGoogle = Number(prompt("What is your hourly wage with Google?"));
+var hourlyWageAmazon = Number(prompt("What is your hourly wage with Amazon?"));
+var hourlyWageFacebook = Number(prompt("What is your hourly wage with Facebook?"));
+var weeklyHoursWorkedGoogle = Number(prompt("How many hours did you work with Google?"));
+var weeklyHoursWorkedAmazon = Number(prompt("How many hours did you work with Amazon?"));
+var weeklyHoursWorkedFacebook = Number(prompt("How many hours did you work with Facebook?"));
+var paycheckGoogle = Number(hourlyWageGoogle * weeklyHoursWorkedGoogle);
+var paycheckAmazon = Number(hourlyWageAmazon * weeklyHoursWorkedAmazon);
+var paycheckFacebook = Number(hourlyWageFacebook * weeklyHoursWorkedFacebook);
+
+alert("You worked a total of " + (weeklyHoursWorkedGoogle +
+weeklyHoursWorkedAmazon + weeklyHoursWorkedFacebook) + " hours and your total pay is "
+    + (paycheckGoogle + paycheckAmazon + paycheckFacebook))
+
