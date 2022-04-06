d6a288b18b0ec919d205c00d0bb5ceee17ccbea1
Ralph <mccloskey.ralph35@gmail.com>
2022-04-06 18:06:55 -0400

    New exercises added

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hello from external JavaScript");
+
+<alert>"Welcome to my Website!"</alert>
+
+var favColor = <prompt>"What is your favorite color?"</prompt>
+
+<alert>"Wow! " + favColor + " is my favorite color too!!!"</alert>
+
