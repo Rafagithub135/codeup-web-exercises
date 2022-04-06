c133420b101bd98e9ea9e2a600715b737355f72f
Ralph <mccloskey.ralph35@gmail.com>
2022-04-06 18:28:13 -0400

    alerts & prompts working

--- js/external.js
+++ js/external.js
@@ -1,9 +1,9 @@
 "use strict";
 console.log("Hello from external JavaScript");
 
-<alert>"Welcome to my Website!"</alert>
+alert("Welcome to my Website!");
 
-var favColor = <prompt>"What is your favorite color?"</prompt>
+var favColor = prompt("What is your favorite color?");
 
-<alert>"Wow! " + favColor + " is my favorite color too!!!"</alert>
+alert("Wow! " + favColor + " is my favorite color too!!!");
 
