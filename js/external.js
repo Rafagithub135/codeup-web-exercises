6da965bf358e34c30919364a072304281c94a12c
Ralph <mccloskey.ralph35@gmail.com>
2022-04-06 18:51:23 -0400

    first exercise completed

--- js/external.js
+++ js/external.js
@@ -7,3 +7,13 @@ var favColor = prompt("What is your favorite color?");
 
 alert("Wow! " + favColor + " is my favorite color too!!!");
 
+var littleMermaidDaysRented = Number(prompt("How many days did you rent The Little Mermaid?"));
+var brotherBearDaysRented = Number(prompt("How many days did you rent Brother Bear?"));
+var herculesDaysRented = Number(prompt("How many days did you rent Hercules?"));
+var pricePerDay = Number(prompt("How much was the rental fee per day?"));
+var totalNumberOfDays = Number(littleMermaidDaysRented + brotherBearDaysRented + herculesDaysRented);
+var totalPaid = Number(pricePerDay * totalNumberOfDays);
+
+alert("Your total number of days you rented movies was " + totalNumberOfDays +
+    " and the total price you paid was $" + totalPaid + ".")
+
