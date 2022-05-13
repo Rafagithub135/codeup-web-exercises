1c9b20d496294d2efd2fb389fad051c50d9b3ffc
Ralph <mccloskey.ralph35@gmail.com>
2022-05-13 13:28:42 -0400

    class selectors completed

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,19 +1,6 @@
 'use strict'
 $(document).ready(function() {
-    $('h1').css('color', '#E95D04');
-
-    $('p').css('text-size', '40px');
-
-    $('#list').HTML('border', '2px solid black');
-
-    $('.list-item').css('background-color', '#486F91');
-
-    $('.division').css('background-color', '#B34242');
-
-    $('.division').innerHTML('This container is closed.');
-
-    $('*').css('border', '2px solid #EEEEEE');
-
+   $('.codeup').css({'border': '1px solid red'});
 
 
 
