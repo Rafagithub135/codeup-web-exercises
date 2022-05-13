da210b5a9bf55523cb30c665e205c444105f9065
Ralph <mccloskey.ralph35@gmail.com>
2022-05-13 13:07:05 -0400

    id selectors entered

---
+++ js/custom-js.js
@@ -0,0 +1,30 @@
+'use strict'
+$(document).ready(function() {
+    $('h1').css('color', '#E95D04');
+
+    $('p').css('text-size', '40px');
+
+    $('#list').HTML('border', '2px solid black');
+
+    $('.list-item').css('background-color', '#486F91');
+
+    $('.division').css('background-color', '#B34242');
+
+    $('.division').innerHTML('This container is closed.');
+
+    $('*').css('border', '2px solid #EEEEEE');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
