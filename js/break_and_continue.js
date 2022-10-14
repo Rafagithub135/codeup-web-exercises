6f2d9e051405f063fc0dd39819a85f79a4ee1ce4
Ralph <mccloskey.ralph35@gmail.com>
2022-10-14 13:30:41 -0400

    break point for lunch

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,7 @@
+"use strict";
 (function() {
 
-    var input = prompt('Enter a odd number between 1 and 50: ');
+    let input = prompt('Enter an odd number between 1 and 50: ');
     while(true) {
         if (input % 2 === 0  || input > 50 || input < 1) {
             alert('You have entered an invalid number.  Please try again.');
@@ -11,10 +12,11 @@
     }
 
     console.log('Number to skip is: ' + input);
-    for(var num = 1; num <= 50; num++) {
+    for(let num = 1; num <= 50; num++) {
         if(num % 2 === 1 && num != input) {
             console.log('Here is an odd number: ' + num);
         } if(num === input){
+            console.log('Yikes! Skipping number ' + input);
             continue;
         }
         if(num > 50) {
