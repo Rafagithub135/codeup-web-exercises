1234df126cd15c4e8780e27c9e2683f1c62e2f81
Ralph <mccloskey.ralph35@gmail.com>
2022-10-14 12:36:32 -0400

    completed 3 of the problems

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,25 +1,52 @@
-(function() {
+"use strict";
+(function () {
 
 
-/* 2. Create a function named showMultiplicationTable that accepts a number and console.logs
-    the multiplication table for that number (just multiply by the numbers 1 through 10).
-*/
+    /* 2. Create a function named showMultiplicationTable that accepts a number and console.logs
+        the multiplication table for that number (just multiply by the numbers 1 through 10).
+    */
     // function showMultiplicationTable(num) {
-    //     for(let i = 1; i <= 10; i++) {
+    //     for (let i = 1; i <= 10; i++) {
     //         let total = i * num;
-    //         console.log(num + " x " + i + " = " +(num * i));
+    //         console.log(num + " x " + i + " = " + (num * i));
     //     }
     // }
-    //
+
     // console.log(showMultiplicationTable(15));
 
-/* 3. Use a for loop and the code from the previous lessons to generate 10 random numbers
-    between 20 and 200 and output to the console whether each number is odd or even.
- */
+    /* 3. Use a for loop and the code from the previous lessons to generate 10 random numbers
+        between 20 and 200 and output to the console whether each number is odd or even.
+     */
 
-    function getRandomInteger(min, max) {
-        Math.floor(Math.random() * (max - min) + min);
+    // function getRandomNumberBetween20And200(max, min) {
+    //     for (let i = 0; i < 10; i++) {
+    //         let random = Math.floor(Math.random() * (max - min) + min);
+    //         console.log(random);
+    //         if (random % 2 === 0) {
+    //             console.log(random + ' is even.');
+    //         } else
+    //             console.log(random + ' is odd.');
+    //     }
+    // }
+
+    // getRandomNumberBetween20And200(201, 20);
+
+    // 4. Create a for loop that uses console.log to create the output shown.
+
+    let pyramidNumber = 1;
+    for (let i = 1; i < 10; i++) {
     }
+    console.log(pyramidNumber);
+
+    // 5. Create a for loop that uses console.log to create the output shown.
+
+    // for (let i = 100; i > 1; i--) {
+    //     if (i % 5 === 0) {
+    //         console.log(i);
+    //     } continue;
+    // }
+
+
 
 
 })();
