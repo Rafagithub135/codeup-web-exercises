49507dd8722dc480583aa832c6d95a6b54139bd7
Ralph <mccloskey.ralph35@gmail.com>
2022-10-14 15:08:04 -0400

    completed first while loop exercise

--- js/while.js
+++ js/while.js
@@ -1,12 +1,24 @@
 "use strict";
-(function() {
-    let num = 2;
-    let count = 0;
-    while(count < 16) {
-        console.log(num);
-        num = num * 2;
-        count = count + 1;
-    }
+(function () {
+
+
+    // let num = 2;
+    // let count = 0;
+    // while (count < 15) {
+    //     console.log(num);
+    //     num = num * 2;
+    //     count = count + 1;
+    // }
+
+    console.log(num);
+
+    // let allConesToSell = Math.floor(Math.random() * 50) + 50;
+    //
+    // do {
+    //     let conesToBeBought = Math.floor(Math.random() * 5) + 1;
+    // } while (allConesToSell > 0) {
+    //     allConesToSell = allConesToSell - conesToBeBought
+    // }
 
 
 })();
