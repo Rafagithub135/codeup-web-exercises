49c1114c408adbfaee33360625a017ba472c1dd2
Ralph <mccloskey.ralph35@gmail.com>
2022-10-17 19:38:09 -0400

    completed planets-string exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,34 +1,57 @@
-// (function () {
-//     "use strict";
-//
-//     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-//
-//     /**
-//      * TODO:
-//      * Convert planetsString to an array, and save it in a variable named
-//      * planetsArray.
-//      * console.log planetsArray to check your work
-//      */
-//
-//     var planetsArray = planetsString.split('| ');
-//     console.log(planetsArray);
-//
-//     /**
-//      * TODO:
-//      * Create a string with <br> tags between each planet. console.log() your
-//      * results. Why might this be useful?
-//      *
-//      * BONUS:
-//      * Create another string that would display your planets in an undordered
-//      * list. You will need an opening AND closing <ul> tags around the entire
-//      * string, and <li> tags around each planet.
-//      */
-//
-//     var planetsList = ['Mercury','<br>','Venus','<br>','Earth','<br>','Mars','<br>','Jupiter','<br>','Saturn','<br>','Uranus','<br>','Neptune'];
-//     console.log(planetsList);
-//
-//
-// var planetsList =
-//     ''<ul>''<li>'Mercury''</li>''<li>''Venus''</li>''<li>''Earth''</li>''<li>''Mars''</li>''<li>''Jupiter''</li>''<li>''Saturn''</li>''<li>''Uranus'</li>''<li>''Neptune''</li>''</ul>';
-//
-// })();
+"use strict";
+(function () {
+
+    let planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+
+    let planetsArray = planetsString.split('|');
+    console.log(planetsArray);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     *
+     * BONUS:
+     * Create another string that would display your planets in an unordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+
+    let planetsList = ['Mercury', '<br>', 'Venus', '<br>', 'Earth', '<br>', 'Mars', '<br>', 'Jupiter', '<br>', 'Saturn', '<br>', 'Uranus', '<br>','Neptune'];
+    console.log(planetsList);
+
+planetsList = '<ul>'
+'<li>'
+'Mercury'
+'</li>'
+'<li>'
+'Venus'
+'</li>'
+'<li>'
+'Earth'
+'</li>'
+'<li>'
+'Mars'
+'</li>'
+'<li>'
+'Jupiter'
+'</li>'
+'<li>'
+'Saturn'
+'</li>'
+'<li>'
+'Uranus'
+'</li>'
+'<li>'
+'Neptune'
+'</li>'
+'</ul>';
+
+})();
