c5b7000ed13ebd38c98ebfed048ceb952ca72515
Ralph <mccloskey.ralph35@gmail.com>
2022-10-21 11:57:08 -0400

    changed welcome html page for css exercise

--- js/warmups.js
+++ js/warmups.js
@@ -9,3 +9,18 @@ function priceOfFruit(obj) {
 }
 
 // console.log(priceOfFruit(fruit));
+
+let str = [
+    'hello',
+    'this is it',
+    'I do not want to be there.',
+]
+
+function getLongestString(str) {
+    let longestString = 0;
+    for (let i = 0; i < str.length - 1; i++) {
+      if (str[i].length > longestString) {
+          console.log(longestString += str[i].length);
+      } continue;
+    }
+}
