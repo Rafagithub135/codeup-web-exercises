16304e1fedad9686213b3f5515afe71604dcf3ad
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2022-11-17 14:10:46 -0500

    modified jquery code

--- js/jquery-intro.js
+++ js/jquery-intro.js
@@ -1,17 +1,5 @@
 $(function () {
-    $("h1").on("click", function (e) {
-        // let randomColor = Math.floor(Math.random() * 16777215).toString();
-        // $(this).css("background-color", "#" + randomColor);
-        e.target.style.backgroundColor = 'goldenrod'
-    })
-    $("p").dblclick(function (e) {
-        e.target.style.fontSize = "18px"
-    })
-    $("li").hover(
-        function () {
-            $(this).css('color', "red");
-        },
-        function () {
-            $(this).css('color', 'black');
-        })
-})
+    let heading = $("#prime").html();
+    alert(heading);
+
+});
