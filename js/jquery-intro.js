d873f9d364cb08887df07f8f044a5b3362b551cd
Ralph <mccloskey.ralph35@gmail.com>
2022-11-18 11:41:32 -0500

    completed jquery-mouse exercise

--- js/jquery-intro.js
+++ js/jquery-intro.js
@@ -1,5 +1,9 @@
 "use strict";
 
 $(function() {
-    alert($("#codeup").html());
+    // alert($("#codeup").html());
+    $("h1").click(function() {
+        $(this).css("background-color", "yellow");
+    }
+
 })
