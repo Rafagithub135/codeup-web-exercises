4f0da39e8e75419abc2af56991b187aeb28c6e4d
Ralph <mccloskey.ralph35@gmail.com>
2022-11-18 16:03:41 -0500

    moved script code to js file

---
+++ js/jquery.js
@@ -0,0 +1,20 @@
+"use strict";
+    $(function() {
+        let konamiCode = ["38", "38", "40", "40", "37", "39", "37", "39", "66", "65", "13"];
+        let count = 0;
+        $(document).keydown(function(e) {
+        let key = e.which;
+        if (key == konamiCode[count]) {
+        count++;
+        if (count == konamiCode.length) {
+        alert("You have added 30 lives!"), $(this).css("background-color", "red");
+        count = 0;
+    }
+    } else {
+        count = 0;
+    }
+    });
+        $(document).keydown(function(event){
+        console.log(event.keyCode);
+    });
+    });
